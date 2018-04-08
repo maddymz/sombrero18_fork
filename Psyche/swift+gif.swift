26ffb03a575c7ddc927b9e2de27888827b9766eb
Rivinis <rivinis@Riviniss-iMac.local>
2018-04-07 20:53:37 -0700

    testing

--- Psyche/swift+gif.swift
+++ Psyche/swift+gif.swift
@@ -1,10 +1,5 @@
 //
-//  Gif.swift
-//  SwiftGif
-//
-//  Created by Arne Bahlo on 07.06.14.
-//  Copyright (c) 2014 Arne Bahlo. All rights reserved.
-//
+
 import UIKit
 import ImageIO
 extension UIImageView {
