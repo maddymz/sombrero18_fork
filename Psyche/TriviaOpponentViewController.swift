fb2a9632f2a070c50409169ff67063df019f9aab
Julia Liu <jbliu1@asu.edu>
2018-04-09 23:32:44 -0700

    added fade in to Trivia Opponent View

--- Psyche/TriviaOpponentViewController.swift
+++ Psyche/TriviaOpponentViewController.swift
@@ -45,29 +45,34 @@ class TriviaOpponentViewController: UIViewController, UITableViewDelegate, UITab
     
     // number of rows in table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.opponentList.opponents.count
+        return self.opponentList.opponents.count + 1
     }
     
     // create a cell for each table view row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let opponent = opponentList.opponents[indexPath.row]
-        
-        if(indexPath.row < level){
-            let cell:UnlockedTriviaOpponentTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "UnlockedOpponentCell") as! UnlockedTriviaOpponentTableViewCell!
-            cell.setOpponent(opponent: opponent)
-            cell.playButton.tag = indexPath.row
+        if(indexPath.row == 0){
+            let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "emptyCell") as! UITableViewCell!
             return cell
         }
         else{
-            let cell:LockedTriviaOpponentTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "LockedOpponentCell") as! LockedTriviaOpponentTableViewCell!
-            if(indexPath.row == level){
-                cell.backgroundView = UIImageView.init(image: UIImage.init(named: "FakeShadowRectangle"))
+            let opponent = opponentList.opponents[indexPath.row-1]
+            
+            if(indexPath.row-1 < level){
+                let cell:UnlockedTriviaOpponentTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "UnlockedOpponentCell") as! UnlockedTriviaOpponentTableViewCell!
+                cell.setOpponent(opponent: opponent)
+                cell.playButton.tag = indexPath.row-1
+                return cell
             }
-            cell.setOpponent(opponent: opponent)
-            return cell
+            else{
+                let cell:LockedTriviaOpponentTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "LockedOpponentCell") as! LockedTriviaOpponentTableViewCell!
+                if(indexPath.row-1 == level){
+                    cell.backgroundView = UIImageView.init(image: UIImage.init(named: "FakeShadowRectangle"))
+                }
+                cell.setOpponent(opponent: opponent)
+                return cell
+            }
         }
-
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
