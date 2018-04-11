cc93bc339b57976b936c76a62a8ea39f7d44085e
Joshua <joshua@Joshuas-Mac.local>
2018-04-11 13:31:24 -0700

    Added alert message before opening hamburger menu links

--- Psyche/GalleryViewController.swift
+++ Psyche/GalleryViewController.swift
@@ -193,19 +193,19 @@ class GalleryViewController: UIViewController, FMMosaicLayoutDelegate, UICollect
     }
     
     @IBAction func openYoutube(_ sender: Any) {
-        menuHelper.openYoutube()
+        menuHelper.openYoutube(vc: self)
     }
     
     @IBAction func openTwitter(_ sender: Any) {
-        menuHelper.openTwitter()
+        menuHelper.openTwitter(vc: self)
     }
     
     @IBAction func openFB(_ sender: Any) {
-        menuHelper.openFB()
+        menuHelper.openFB(vc: self)
     }
     
     @IBAction func openInsta(_ sender: Any) {
-        menuHelper.openInsta()
+        menuHelper.openInsta(vc: self)
     }
 }
 
--- Psyche/sideMenuHelper.swift
+++ Psyche/sideMenuHelper.swift
@@ -40,60 +40,61 @@ class menuHelper{
         })
     }
     
-    static func openYoutube(){
+    static func openYoutube(vc : UIViewController){
         let YoutubeUser =  "UC2BGcbPW8mxryXnjQcBqk6A"
-        let appURL = NSURL(string: "youtube://www.youtube.com/user/\(YoutubeUser)")!
-        let webURL = NSURL(string: "https://www.youtube.com/channel/\(YoutubeUser)")!
-        let application = UIApplication.shared
         
-        if application.canOpenURL(appURL as URL) {
-            application.open(appURL as URL)
-        } else {
-            // if Youtube app is not installed, open URL inside Safari
-            application.open(webURL as URL)
-        }
+        showWarningAlert(vc: vc, appURLStr: "youtube://www.youtube.com/user/\(YoutubeUser)", webURLStr: "https://www.youtube.com/channel/\(YoutubeUser)")
     }
     
-    static func openTwitter(){
+    static func openTwitter(vc : UIViewController){
         let screenName =  "nasapsyche"
-        let appURL = NSURL(string: "twitter://user?screen_name=\(screenName)")!
-        let webURL = NSURL(string: "https://twitter.com/\(screenName)")!
         
-        let application = UIApplication.shared
-        
-        if application.canOpenURL(appURL as URL) {
-            application.open(appURL as URL)
-        } else {
-            application.open(webURL as URL)
-        }
+        showWarningAlert(vc: vc, appURLStr: "twitter://user?screen_name=\(screenName)", webURLStr: "https://twitter.com/\(screenName)")
     }
     
-    static func openFB(){
+    static func openFB(vc : UIViewController){
+        let Username =  "nasapsyche"
+        
+        showWarningAlert(vc: vc, appURLStr: "fb://profile/\(Username)", webURLStr: "https://facebook.com/\(Username)")
+    }
+    
+    static func openInsta(vc : UIViewController){
         let Username =  "nasapsyche" // Your Instagram Username here
-        let appURL = NSURL(string: "fb://profile/\(Username)")!
-        let webURL = NSURL(string: "https://facebook.com/\(Username)")!
+        
+        showWarningAlert(vc: vc, appURLStr: "instagram://user?username=\(Username)", webURLStr: "https://instagram.com/\(Username)")
+    }
+    
+    // Try opening the app, if the app is not installed then open the website in Safari
+    static func openAppOrWeb(appURLStr: String, webURLStr: String) {
+        let appURL = NSURL(string: appURLStr)!
+        let webURL = NSURL(string: webURLStr)!
         let application = UIApplication.shared
         
         if application.canOpenURL(appURL as URL) {
             application.open(appURL as URL)
         } else {
-            // if Instagram app is not installed, open URL inside Safari
+            // If app is not installed, open URL in Safari
             application.open(webURL as URL)
         }
     }
     
-    static func openInsta(){
-        let Username =  "nasapsyche" // Your Instagram Username here
-        let appURL = NSURL(string: "instagram://user?username=\(Username)")!
-        let webURL = NSURL(string: "https://instagram.com/\(Username)")!
-        let application = UIApplication.shared
+    static func showWarningAlert(vc: UIViewController, appURLStr: String, webURLStr: String) {
+        // Declare Alert message
+        let dialogMessage = UIAlertController(title: "Leave Psyche?", message: "This app is trying to open an app outside of Psyche. Are you sure you want to open it?", preferredStyle: .alert)
         
-        if application.canOpenURL(appURL as URL) {
-            application.open(appURL as URL)
-        } else {
-            // if Instagram app is not installed, open URL inside Safari
-            application.open(webURL as URL)
-        }
+        // Create OK button with action handler
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+            openAppOrWeb(appURLStr: appURLStr, webURLStr: webURLStr)
+        })
+        
+        // Create Cancel button with action handlder
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) -> Void in }
+        
+        //Add OK and Cancel button to dialog message
+        dialogMessage.addAction(ok)
+        dialogMessage.addAction(cancel)
+        
+        // Present dialog message in ViewController passed in
+        vc.present(dialogMessage, animated: true, completion: nil)
     }
-    
 }
