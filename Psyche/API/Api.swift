be1ca6acdf9cd442977fdf5c904cc8de734c9218
Madhukar Raj <mraj2@sese-admins-air.3500.dhcp.asu.edu>
2019-01-23 17:48:02 -0700

    Added API folder Recommiting gallery data as position in repository is changed

---
+++ Psyche/API/Api.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Psyche
+//
+//  Created by Madhukar Raj on 1/16/19.
+//  Copyright © 2019 ASU. All rights reserved.
+//
+
+import Foundation
+
+
+struct apiUrls {
+    let galleryApi = "https://test-psyche.ws.asu.edu/wp-json/psyche/v1/gallery"
+}
+
