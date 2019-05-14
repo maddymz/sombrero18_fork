746a17bf281f63fb1815aa7528082115c27dbbad
maddymz <madhukar4u.rm@gmail.com>
2019-05-13 17:54:45 -0700

    added social media view

---
+++ Psyche/Social/SocialMediaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SocialMediaViewController.swift
+//  Psyche
+//
+//  Created by psyche-admin on 5/13/19.
+//  Copyright © 2019 ASU. All rights reserved.
+//
+
+import UIKit
+
+class SocialMediaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
